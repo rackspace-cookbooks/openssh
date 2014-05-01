5e74fb9ad813eace5fda5a8902e1c411f19e08c9
Jason Nelson <schplat@gmail.com>
2014-05-01 09:17:09 -0500

    Fixes #4

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Rackspace, US Inc.'
 maintainer_email  'rackspace-cookbooks@rackspace.com'
 license           'Apache 2.0'
 description       'Installs openssh'
-version           '3.1.0'
+version           '3.1.1'
 
 recipe 'rackspace_openssh', 'Installs openssh'
 
